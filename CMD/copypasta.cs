434ec8a70d1071cf80a95da0a4761b44c0460414
Solarys <realshinychu@gmail.com>
2018-05-14 09:11:03 -0700

    New Command: >>work

---
+++ CMD/copypasta.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace ZeroTwo.Commands
+{
+    public class CopyPasta : ModuleBase<SocketCommandContext>
+    {
+        [Command("work")]
+        public async Task WorkPasta(string lang1 = null, string lang2 = null, string lang3 = null, string lang4 = null)
+        {
+            if(lang1 == null || lang2 == null || lang3 == null || lang4 == null)
+            {
+                await ReplyAsync("```\n>>work <language 1> <language 2> <language 3> <language 4>\n```");
+                return;
+            }
+
+            await ReplyAsync($"My work is {lang1} development, sometimes {lang2}, sometimes {lang3}, sometimes {lang4} - mostly {lang1} tho");
+        }
+    }
+}
